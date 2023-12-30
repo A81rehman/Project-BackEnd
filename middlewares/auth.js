a0d80fb43153005fb00f31f45b1898dfd7cb7e6a
A81rehman <ar652796@gmail.com>
2023-12-30 16:13:13 +0500

    Add backend files

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,12 +4,11 @@ let { Users } = require("../models/user");
 
 async function auth(req, res, next) {
 let token = req.headers['x-auth-token']; 
-if (!token) return res.status(400).send('Token missing');
+if (!token) return res.status(401).send('Token missing');
 try {
     let User = jwt.verify(token, config.get("jwtPrivateKey"));
     req.User = await Users.findById(User._id);
 } catch (error) {
-    window.alert("You are not authorised to access this. Please Login to your account!")
     return res.status(401).send('Invalid Token');
 }
 next();
